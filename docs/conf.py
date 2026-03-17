33116cb0929fde1f6db8ff9e774d9fbbe7d6cb9d
robertjunior2154 <robertjunior2154@outlook.com>
2026-03-17 17:32:19 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,17 +2,23 @@ project = 'hulu-login-account'
 author = 'hulu-login-account'
 release = '1.0'
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Theme
 html_theme = 'alabaster'
-html_static_path = ['_static']   
+html_static_path = ['_static']
 
-html_js_files = [
-    'chatbot.js',
-]
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
 html_favicon = '_static/favicon.png'
+
 # Google & Bing Verification Meta Tags
 html_context = {
     "meta_tags": """
@@ -20,3 +26,6 @@ html_context = {
     <meta name="msvalidate.01" content="739245F5D54BCBF40AC056DC0CBF5710" />
     """
 }
+
+# Base URL for sitemap
+html_baseurl = 'https://hulutvhelp.readthedocs.io/en/latest/'
